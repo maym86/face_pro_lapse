29db8dd33f8e6552933d6ad2a5251a4d73b459d3
maym86 <maym86@gmail.com>
2014-05-14 14:47:49 -0700

    Refactored code
    Added functions

--- SelfieVideo.py
+++ SelfieVideo.py
@@ -6,16 +6,6 @@ import glob
 import numpy as np
 import math
 
-def HistEqualisationColour(img):
-
-    ycrcb = cv2.cvtColor(img, cv.CV_BGR2YCrCb)
-    y,Cr,Cb = cv2.split(ycrcb)
-
-    y = cv2.equalizeHist(y);
-    ycrcb = cv2.merge((y,Cr,Cb))
-
-    return cv2.cvtColor(ycrcb,cv.CV_YCrCb2BGR);    
-
 box = []
 drawingBox = False
 
@@ -30,11 +20,64 @@ def OnMouse(event, x, y, flags, params):
         print 'End Mouse Position: '+str(x)+', '+str(y)
         box = (box[0], box[1] , x - box[0] ,y - box[1])        
         print box
-    if event == cv.CV_EVENT_MOUSEMOVE and drawingBox:
+    elif event == cv.CV_EVENT_MOUSEMOVE and drawingBox:
         box = (box[0], box[1] , x - box[0] ,y - box[1])
         
+def SelectFaceManually(img):
+    global box
+    cv2.namedWindow('Click Face')
+    cv.SetMouseCallback('Click Face', OnMouse, 0)
+    while(1):
+        temp = img.copy()
+        if box:
+            cv2.rectangle(temp, (box[0],box[1]) , (box[0] + box[2], box[1] + box[3]),(0,0,255), 2)
+        cv2.imshow('Click Face', temp)
+        if cv2.waitKey(1) == 27:
+            cv2.destroyAllWindows()
+            break            
+    face = box
+    box = []
+    return face
 
-        
+
+def FindLargestFace(gray, faceCascades):
+    # find faces in the image using all possible Haar cascades
+    faces = []
+    for cascade in faceCascades:
+        f = cascade.detectMultiScale(gray, minSize=(200,100))
+        if len(f) > 0:
+            if len(faces) == 0:
+                faces = f
+            else:
+                faces = np.concatenate((faces,f))    
+
+    #if no face is found manually select face
+    if len(faces) == 0:
+        face = SelectFaceManually(img)            
+    else:
+        #Sort to get the largest face  
+        face = sorted(faces, key=lambda x: x[3])[-1]
+
+    return face
+
+def FindEyeAngle(gray, face):
+    (x,y,w,h) = face
+    #try to find eyes to get head rotation
+    faceROIGray = gray[y:y+h, x:x+w]
+    eyes = eye_cascade.detectMultiScale(faceROIGray)
+    if len(eyes) >= 2:
+        eyeAngle = math.atan((eyes[0][1] - eyes[1][1])/(eyes[0][0] - eyes[1][0]))
+    else:
+        eyeAngle = 0        
+    return eyeAngle
+
+def HistEqualisationColour(img):
+    ycrcb = cv2.cvtColor(img, cv.CV_BGR2YCrCb)
+    y,Cr,Cb = cv2.split(ycrcb)
+    y = cv2.equalizeHist(y);
+    ycrcb = cv2.merge((y,Cr,Cb))
+    return cv2.cvtColor(ycrcb,cv.CV_YCrCb2BGR);
+
 
 if __name__ == "__main__":
     imgs = glob.glob("images/*.jpg")
@@ -44,11 +87,11 @@ if __name__ == "__main__":
     videoSize = (1280 ,720)
     centre = (videoSize[0]/2,videoSize[1]/2)
     
-    face_cascade = []
+    faceCascades = []
 
-    face_cascade.append(cv2.CascadeClassifier('haarcascade_frontalface_alt.xml'))
-    face_cascade.append(cv2.CascadeClassifier('haarcascade_frontalface_default.xml'))
-    face_cascade.append(cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml'))
+    faceCascades.append(cv2.CascadeClassifier('haarcascade_frontalface_alt.xml'))
+    faceCascades.append(cv2.CascadeClassifier('haarcascade_frontalface_default.xml'))
+    faceCascades.append(cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml'))
 
     eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
     
@@ -57,51 +100,13 @@ if __name__ == "__main__":
     
     for fname in imgs:
         img = cv2.imread(fname)
-
         img = cv2.resize(img, (0,0), fx=0.5, fy=0.5)
         #img = HistEqualisationColour(img)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        # find faces in the image using all possible Haar cascades
-        faces = []
-        for cascade in face_cascade:
-            f = cascade.detectMultiScale(gray, minSize=(200,100))
-            if len(f) > 0:
-                if len(faces) == 0:
-                    faces = f
-                else:
-                    faces = np.concatenate((faces,f))    
+        face = FindLargestFace(gray, faceCascades)
 
-        #if no face is found manually select face
-        if len(faces) == 0:
-            cv2.namedWindow('Click Face')
-            cv.SetMouseCallback('Click Face', OnMouse, 0)
-            while(1):
-                temp = img.copy()
-                if box:
-                    cv2.rectangle(temp, (box[0],box[1]) , (box[0] + box[2], box[1] + box[3]),(0,0,255), 2)
-                cv2.imshow('Click Face', temp)
-                if cv2.waitKey(1) == 27:
-                    cv2.destroyAllWindows()
-                    break
-            
-            face = box
-            box = []
-            
-        else:
-            #Sort to get the largest face  
-            face = sorted(faces, key=lambda x: x[3])[-1]      
-
-        (x,y,w,h) = face
-
-
-        #try to find eyes to get head rotation
-        roi_gray = gray[y:y+h, x:x+w]
-        eyes = eye_cascade.detectMultiScale(roi_gray)
-        if len(eyes) >= 2:
-            eyeAngle = math.atan((eyes[0][1] - eyes[1][1])/(eyes[0][0] - eyes[1][0]))
-        else:
-            eyeAngle = 0
+        eyeAngle = FindEyeAngle(gray, face)
 
 
         #rescale to desired face size  
@@ -109,7 +114,7 @@ if __name__ == "__main__":
         scaled = cv2.resize(img, (0,0), fx=scale, fy=scale)
 
         #rescale face rect
-        (x,y,w,h) = tuple([scale*x for x in face])
+        (x,y,w,h) = [scale*x for x in face]
 
         #move the image so the face is centred
         M = np.float32([[1,0,(centre[0] -w/2) - x],[0,1,(centre[1] -h/2) - y]])
@@ -123,7 +128,7 @@ if __name__ == "__main__":
         #write to video file      
         video.write(out)
         cv2.imshow('face',out)
-        cv2.waitKey(100)
+        cv2.waitKey(1)
         
 
     video.release()
